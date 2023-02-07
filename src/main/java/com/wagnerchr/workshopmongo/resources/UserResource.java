f609e5483a98401809e3ab10f7ed0181073405cd
WagnerChr <wagnerckv@gmail.com>
2023-02-07 10:49:58 -0300

    User insert

--- src/main/java/com/wagnerchr/workshopmongo/resources/UserResource.java
+++ src/main/java/com/wagnerchr/workshopmongo/resources/UserResource.java
@@ -1,5 +1,6 @@
 package com.wagnerchr.workshopmongo.resources;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -7,8 +8,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.wagnerchr.workshopmongo.domain.User;
 import com.wagnerchr.workshopmongo.dto.UserDTO;
@@ -34,6 +38,15 @@ public class UserResource { // Access Service
 	public ResponseEntity<UserDTO> findById(@PathVariable String id) {
 		User obj = service.findById(id);
 		return ResponseEntity.ok().body(new UserDTO(obj));
+	}
+	
+	// Insert new User
+	@PostMapping
+	public ResponseEntity<Void> insert(@RequestBody UserDTO objDto) {
+		User obj = service.fromDTO(objDto);
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
 	}
 	
 }
--- src/main/java/com/wagnerchr/workshopmongo/services/UserService.java
+++ src/main/java/com/wagnerchr/workshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.wagnerchr.workshopmongo.domain.User;
+import com.wagnerchr.workshopmongo.dto.UserDTO;
 import com.wagnerchr.workshopmongo.repositories.UserRepository;
 import com.wagnerchr.workshopmongo.services.exceptions.ObjectNotFoundException;
 
@@ -24,4 +25,13 @@ public class UserService { // Access Repository
 		Optional<User> obj = repo.findById(id);
 		return obj.orElseThrow( () -> new ObjectNotFoundException("Objeto não encontrado") );
 	}
+	
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDTO) {
+		return new User(objDTO.getId(), objDTO.getName(), objDTO.getEmail());
+	}
+	
 }
