7d8939f82c900ea26aa9a9fd66d68f4d4033cefb
WagnerChr <wagnerckv@gmail.com>
2023-02-08 17:16:18 -0300

    Query fullsearch

--- src/main/java/com/wagnerchr/workshopmongo/resources/PostResource.java
+++ src/main/java/com/wagnerchr/workshopmongo/resources/PostResource.java
@@ -1,5 +1,6 @@
 package com.wagnerchr.workshopmongo.resources;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -34,4 +35,17 @@ public class PostResource { // Access Service
 		List<Post> list = service.findByTitle(text);
 		return ResponseEntity.ok().body(list);
 	}	
+	
+	@GetMapping(value="/fullsearch")
+	public ResponseEntity<List<Post>> findByTitle(
+			@RequestParam(value="text", defaultValue="") String text,
+			@RequestParam(value="minDate", defaultValue="") String minDate,
+			@RequestParam(value="maxDate", defaultValue="") String maxDate) {
+		text = URL.decodeParam(text);
+		Date min = URL.convertDate(minDate, new Date(0L));
+		Date max = URL.convertDate(maxDate, new Date());
+		List<Post> list = service.fullSearch(text, min, max);
+		return ResponseEntity.ok().body(list);
+	}	
+	
 }
--- src/main/java/com/wagnerchr/workshopmongo/services/PostService.java
+++ src/main/java/com/wagnerchr/workshopmongo/services/PostService.java
@@ -1,5 +1,6 @@
 package com.wagnerchr.workshopmongo.services;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -25,4 +26,9 @@ public class PostService { // Access Repository
 		//return repo.findByTitleContainingIgnoreCase(text);
 		return repo.searchTitle(text);
 	}  
+	
+	public List<Post> fullSearch(String text, Date minDate, Date maxDate) {
+		maxDate = new Date(maxDate.getTime() + 24 * 60  * 60 * 1000);
+		return repo.fullSearch(text, minDate, maxDate);
+	}
 }
