b1795614615aad75af0a8af5330d42f8f27c48c8
WagnerChr <wagnerckv@gmail.com>
2023-02-08 16:23:47 -0300

    Comments in Posts

--- src/main/java/com/wagnerchr/workshopmongo/config/Instantiation.java
+++ src/main/java/com/wagnerchr/workshopmongo/config/Instantiation.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
 import com.wagnerchr.workshopmongo.domain.Post;
 import com.wagnerchr.workshopmongo.domain.User;
 import com.wagnerchr.workshopmongo.dto.AuthorDTO;
+import com.wagnerchr.workshopmongo.dto.CommentDTO;
 import com.wagnerchr.workshopmongo.repositories.PostRepository;
 import com.wagnerchr.workshopmongo.repositories.UserRepository;
 
@@ -41,6 +42,14 @@ public class Instantiation implements CommandLineRunner{
 		
 		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem!", "Vou viajar", new AuthorDTO(maria));
 		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia!", "Acordei Feliz!!!!", new AuthorDTO(maria));
+		
+		CommentDTO comment01 = new CommentDTO("Boa viagem mano!", sdf.parse("21/03/2018"), new AuthorDTO(alex));
+		CommentDTO comment02 = new CommentDTO("Aproveite!", sdf.parse("22/03/2018"), new AuthorDTO(bob));
+		CommentDTO comment03 = new CommentDTO("Tenha um ótimo dia", sdf.parse("23/03/2018"), new AuthorDTO(alex));
+		
+		post1.getComments().addAll(Arrays.asList(comment01, comment02));
+		post2.getComments().addAll(Arrays.asList(comment03));
+		
 		
 		userRepository.saveAll(Arrays.asList(maria, alex, bob));
 		postRepository.saveAll(Arrays.asList(post1, post2));
--- src/main/java/com/wagnerchr/workshopmongo/domain/Post.java
+++ src/main/java/com/wagnerchr/workshopmongo/domain/Post.java
@@ -1,13 +1,16 @@
 package com.wagnerchr.workshopmongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Objects;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import com.wagnerchr.workshopmongo.dto.AuthorDTO;
+import com.wagnerchr.workshopmongo.dto.CommentDTO;
 
 @Document	
 public class Post implements Serializable {
@@ -19,6 +22,8 @@ public class Post implements Serializable {
 	private String title;
 	private String body;
 	private AuthorDTO author;
+	
+	private List<CommentDTO> comments = new ArrayList<>();
 	
 	
 	public Post() {
@@ -73,6 +78,14 @@ public class Post implements Serializable {
 	public void setAuthor(AuthorDTO author) {
 		this.author = author;
 	}
+	
+	public List<CommentDTO> getComments() {
+		return comments;
+	}
+
+	public void setComments(List<CommentDTO> comments) {
+		this.comments = comments;
+	}
 
 	@Override
 	public int hashCode() {
@@ -90,6 +103,4 @@ public class Post implements Serializable {
 		Post other = (Post) obj;
 		return Objects.equals(id, other.id);
 	}
-
-	
 }
